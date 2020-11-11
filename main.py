efa4117f1055b465036dbf13e4939f9a9f1f05a3
Kallemange <33366570+Kallemange@users.noreply.github.com>
2020-11-11 14:06:46 +0100

    Update main.py

--- main.py
+++ main.py
@@ -3,22 +3,51 @@ import os
 
 
 #from wand.image import image
-conn = pyodbc.connect('Driver={SQL Server};'
-                      'Server=Solpmsql2;'
-                      'Database=arvika_husar_20201110;'
-                      'Trusted_Connection=yes;')
+class Config():
+    def __init__(self):
+        with open('config.txt','r') as file:
+            content = file.readlines()
+            self.server = content[0].strip()
+            self.database = content[1].strip()
+            self.tables = content[2].split()
+    def tostr(self):
+        return 'Driver={SQL Server};Server='+self.server +';Database='+self.database +';Trusted_Connection=yes;'
 
-dir = 'dk_kamerabildserviceorderrad'
-cursor = conn.cursor()
-cursor.execute('SELECT top 1 DK_kamerabildId,radnr, bild, notering FROM ' + dir)
-rows = cursor.fetchall()
+def getSQLStr(tabell):
+    tabell = tabell.strip()
+    topX = ' top 10 '
+    if (tabell.lower() == 'dk_kamerabildbesiktning'):
+        return ('SELECT' +topX+ ' DK_kamerabildId,lopnr, bild, notering FROM ' + tabell)
+
+    elif(tabell.lower() == 'dk_kamerabildbesiktrad'):
+        return 'SELECT' +topX+ ' DK_kamerabildId,radlopnr AS lopnr, bild, notering FROM ' + tabell
+
+    elif(tabell.lower() == 'dk_kamerabildserviceorderrad'):
+        return 'SELECT' +topX+ ' DK_kamerabildId,radnr lopnr, bild, notering FROM ' + tabell
 
 
-try:
-    os.mkdir(dir)
-except Exception:
-    pass
+def execSQL(config, conn):
+    cursor = conn.cursor()
+    for tabell in config.tables:
+        SQLStr = getSQLStr(tabell)
+        cursor.execute(SQLStr)
+        rows = cursor.fetchall()
+        for row in rows:
+            print(str(row.DK_kamerabildId) + ", " + str(row.lopnr) + ", " + row.notering)
 
-for row in rows:
-    filnamn = dir +'/' + str(row.DK_kamerabildId)+"_" +str(row.radnr)
-    print(row)
+
+def main():
+    config = Config()
+    conn = pyodbc.connect(config.tostr())
+
+    execSQL(config, conn)
+    try:
+        os.mkdir(dir)
+    except Exception:
+        pass
+
+    #for row in rows:
+    #    filnamn = dir +'/' + str(row.DK_kamerabildId)+"_" +str(row.radnr)
+        #print(row)
+
+main()
